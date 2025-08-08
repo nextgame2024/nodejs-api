57cc21c29f1a84257486bb71395d4af2b3eee17e
nextgame2024 <nextgamebrisbane@gmail.com>
2025-08-08 15:48:31 +1000

    New endpoints fixed

--- src/routes/article.routes.js
+++ src/routes/article.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { listArticles } from "../controllers/article.controller.js";
+import { getFeed } from "../controllers/feed.controller.js";
+import { authRequired } from "../middlewares/authJwt.js";
 
 const router = Router();
 router.get("/articles", listArticles);
