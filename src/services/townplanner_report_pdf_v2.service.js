d26a9a1046aa42336b881cabfd01e933d38b52b4
nextgame2024 <nextgamebrisbane@gmail.com>
2026-02-21 11:31:11 +1000

    Overlays airport 8

--- src/services/townplanner_report_pdf_v2.service.js
+++ src/services/townplanner_report_pdf_v2.service.js
@@ -804,7 +804,7 @@ export async function buildTownPlannerReportPdfV2(
     const hasAirportOverlayLayers =
       Array.isArray(airportOverlayLayers) && airportOverlayLayers.length > 0;
     const mapCenter = isAirportOverlay
-      ? nudgeCenterLng(center, 0.0066)
+      ? nudgeCenterLng(center, 0.0086)
       : center;
 
     let mapBuffer =
