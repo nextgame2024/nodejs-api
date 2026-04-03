d73067963e04ec332c2c9c812a45eae209fdf542
nextgame2024 <nextgamebrisbane@gmail.com>
2026-04-03 13:28:36 +1000

    Map background adjustment 1

--- src/services/townplanner_report_pdf_v2.service.js
+++ src/services/townplanner_report_pdf_v2.service.js
@@ -6,7 +6,7 @@ import {
   getParcelOverlayMapImageBufferV2,
 } from "./googleStaticMaps_v2.service.js";
 
-export const PDF_ENGINE_VERSION = "TPR-PDFKIT-V3-2026-04-03.73";
+export const PDF_ENGINE_VERSION = "TPR-PDFKIT-V3-2026-04-03.74";
 
 const VEGETATION_STATE_MAPPING_CODE =
   "state_mapping_sara_regulated_vegetation_management_map";
